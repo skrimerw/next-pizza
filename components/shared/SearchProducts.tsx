450409f5f771864e22e1103f4656aae11550e3bd
skrimerw <skrimerw@gmail.com>
2025-06-05 21:30:36 +0300

    add arrow control to search bar

--- components/shared/SearchProducts.tsx
+++ components/shared/SearchProducts.tsx
@@ -8,115 +8,181 @@ import { axiosInstance } from "@/lib/axiosInstance";
 import { cn, markSubstr } from "@/lib/utils";
 import { useAppContext } from "@/contexts/AppContextProvider";
 import SearchFocusedOverlay from "./SearchFocusedOverlay";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function SearchProducts() {
-  const [searchValue, setSearchValue] = useState("");
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(false);
-  const { isSearchFocused, setIsSearchFocused } = useAppContext();
-  const searchInput = useRef<HTMLInputElement>(null);
+    const [searchValue, setSearchValue] = useState("");
+    const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(false);
+    const { isSearchFocused, setIsSearchFocused } = useAppContext();
+    const searchInput = useRef<HTMLInputElement>(null);
+    const [currentProduct, setCurrentProduct] = useState<number | null>(null);
+    const router = useRouter();
 
-  useEffect(() => {
-    if (isSearchFocused && searchValue) {
-      setLoading(true);
+    useEffect(() => {
+        if (isSearchFocused && searchValue && currentProduct === null) {
+            setLoading(true);
 
-      async function fetchData() {
-        try {
-          const { data } = await axiosInstance.get("/products/search", {
-            params: { query: searchValue },
-          });
+            async function fetchData() {
+                try {
+                    const { data } = await axiosInstance.get(
+                        "/products/search",
+                        {
+                            params: { query: searchValue },
+                        }
+                    );
 
-          setProducts(data);
-        } catch (e) {
-          console.error(e);
-        } finally {
-          setLoading(false);
+                    setProducts(data);
+                } catch (e) {
+                    console.error(e);
+                } finally {
+                    setLoading(false);
+                }
+            }
+            fetchData();
         }
-      }
-      fetchData();
+    }, [searchValue]);
+
+    function resetSearch() {
+        setSearchValue("");
+        setIsSearchFocused(false);
+        searchInput.current?.blur();
     }
-  }, [searchValue]);
 
-  return (
-    <>
-      <SearchFocusedOverlay
-        onClick={() => {
-          setIsSearchFocused(false);
-        }}
-      />
-      <div className={cn("search-input-container w-full relative z-50")}>
-        <Search
-          size={16}
-          className="absolute top-1/2 left-3 -translate-y-1/2 text-gray-400"
-        />
-        {searchValue.length > 0 && (
-          <label
-            htmlFor="search-products-input"
-            onClick={() => {
-              setSearchValue("");
+    function handleArrowPress(e: React.KeyboardEvent<HTMLInputElement>) {
+        let newCurrentProduct = currentProduct;
+        switch (e.key) {
+            case "ArrowDown":
+                if (
+                    currentProduct === null ||
+                    currentProduct === products.length - 1
+                ) {
+                    newCurrentProduct = 0;
+                } else {
+                    newCurrentProduct = currentProduct + 1;
+                }
 
-              searchInput.current?.focus();
-            }}
-          >
-            <X
-              size={16}
-              className="absolute top-1/2 right-3 -translate-y-1/2 text-gray-400 cursor-pointer"
+                break;
+            case "ArrowUp":
+                if (!currentProduct) {
+                    newCurrentProduct = products.length - 1;
+                } else {
+                    newCurrentProduct = currentProduct - 1;
+                }
+
+                break;
+            case "Enter":
+                if (currentProduct !== null) {
+                    router.push(`/products/${products[currentProduct].id}`);
+
+                    resetSearch();
+                }
+
+                break;
+        }
+
+        if (newCurrentProduct !== null && searchInput.current) {
+            setCurrentProduct(newCurrentProduct);
+            setSearchValue(products[newCurrentProduct].name);
+
+            searchInput.current.selectionEnd =
+                products[newCurrentProduct].name.length;
+        }
+    }
+
+    return (
+        <>
+            <SearchFocusedOverlay
+                onClick={() => {
+                    setIsSearchFocused(false);
+                }}
             />
-          </label>
-        )}
-        <Input
-          ref={searchInput}
-          id="search-products-input"
-          className="h-[40px] bg-gray-50 border-none rounded-xl px-9 placeholder:text-gray-400 focus-visible:ring-0"
-          placeholder="Поиск пиццы..."
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-          onFocus={() => setIsSearchFocused(true)}
-        />
-        {((loading && isSearchFocused) ||
-          (searchValue.length > 0 && isSearchFocused)) && (
-          <>
-            <div className="absolute left-0 right-0 top-12 rounded-xl min-h-16 bg-white z-20 overflow-hidden shadow-md">
-              <div
-                className={`absolute h-full w-full bg-white/60 flex transition-all items-center justify-center overflow-hidden ${
-                  loading ? "opacity-100 visible" : "opacity-0 invisible"
-                }`}
-              >
-                <LoaderCircle
-                  size={30}
-                  className="text-gray-400 animate-spin"
+            <div className={cn("search-input-container w-full relative z-50")}>
+                <Search
+                    size={16}
+                    className="absolute top-1/2 left-3 -translate-y-1/2 text-gray-400"
                 />
-              </div>
-              {products.length === 0 && !loading ? (
-                <span className="text-center block mt-5 text-gray-500">
-                  Ничего не найдено
-                </span>
-              ) : (
-                products.map((product) => {
-                  return (
-                    <div key={product.id}>
-                      <a
-                        href={`/products/${product.id}`}
-                        className="flex items-center px-4 py-2 gap-4 transition-all hover:bg-gray-50"
-                      >
-                        <img src={product.imageUrl} className="h-10" />
-                        <span
-                          dangerouslySetInnerHTML={{
-                            __html: markSubstr(
-                              product.name,
-                              searchValue.trim()
-                            ),
-                          }}
-                        ></span>
-                      </a>
-                    </div>
-                  );
-                })
-              )}
+                {searchValue.length > 0 && (
+                    <X
+                        onClick={() => {
+                            setSearchValue("");
+
+                            searchInput.current?.focus();
+                        }}
+                        size={16}
+                        className="absolute top-1/2 right-3 -translate-y-1/2 text-gray-400 cursor-pointer"
+                    />
+                )}
+                <Input
+                    ref={searchInput}
+                    className={cn(
+                        "h-[40px] bg-gray-50 border-none rounded-xl px-9 placeholder:text-gray-400 focus-visible:ring-0",
+                        isSearchFocused && "bg-white"
+                    )}
+                    placeholder="Поиск пиццы..."
+                    value={searchValue}
+                    onChange={(e) => {
+                        setSearchValue(e.target.value);
+                        setCurrentProduct(null);
+                    }}
+                    onFocus={() => setIsSearchFocused(true)}
+                    onKeyDown={(e) => handleArrowPress(e)}
+                />
+                {((loading && isSearchFocused) ||
+                    (searchValue.length > 0 && isSearchFocused)) && (
+                    <>
+                        <div className="absolute left-0 right-0 top-12 rounded-xl min-h-14 bg-white z-20 overflow-hidden shadow-md">
+                            <div
+                                className={`absolute h-full w-full bg-white/60 flex transition-all items-center justify-center overflow-hidden ${
+                                    loading
+                                        ? "opacity-100 visible"
+                                        : "opacity-0 invisible"
+                                }`}
+                            >
+                                <LoaderCircle
+                                    size={30}
+                                    className="text-gray-400 animate-spin"
+                                />
+                            </div>
+                            {products.length === 0 && !loading ? (
+                                <span className="text-center block mt-4 text-gray-500">
+                                    Ничего не найдено
+                                </span>
+                            ) : (
+                                products.map((product, index) => {
+                                    return (
+                                        <div key={product.id}>
+                                            <Link
+                                                onClick={resetSearch}
+                                                href={`/products/${product.id}`}
+                                                className={cn(
+                                                    "flex items-center px-4 py-2 gap-4 transition-all hover:bg-gray-100",
+                                                    currentProduct === index &&
+                                                        "bg-gray-100 hover:bg-gray-200"
+                                                )}
+                                            >
+                                                <img
+                                                    src={product.imageUrl}
+                                                    className="h-10"
+                                                />
+                                                <span
+                                                    dangerouslySetInnerHTML={{
+                                                        __html: markSubstr(
+                                                            product.name,
+                                                            searchValue.trim()
+                                                        ),
+                                                    }}
+                                                ></span>
+                                            </Link>
+                                        </div>
+                                    );
+                                })
+                            )}
+                        </div>
+                    </>
+                )}
             </div>
-          </>
-        )}
-      </div>
-    </>
-  );
+        </>
+    );
 }
